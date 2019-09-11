5887115d6414ccfdfc6c41313de9c92c8c2cac23
LarryN3t <lophone75@gmail.com>
2019-09-11 21:53:30 +0200

    Aggiunto librerie bmp28

--- main/main.c
+++ main/main.c
@@ -7,6 +7,11 @@
 
 #include "lmic.h"
 
+#include "bmp280.h"
+
+#define SDA_GPIO GPIO_NUM_21
+#define SCL_GPIO GPIO_NUM_22
+
 //u1_t NWKSKEY[16] = { 0xa2, 0x25, 0xb5, 0x22, 0x8f, 0xe5, 0xb8, 0x18, 0xb6, 0x19, 0xc1, 0xb0, 0x9b, 0xc8, 0xdb, 0xd2 };
 //u1_t APPSKEY[16] = { 0xfa, 0x4c, 0xd4, 0xd2, 0x78, 0x0d, 0x77, 0xc1, 0x03, 0x82, 0xab, 0x77, 0xdb, 0xd8, 0xe7, 0x00 };
 //u4_t DEVADDR = 0x07a56a5e;
@@ -28,7 +33,7 @@ void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
 
-static uint8_t mydata[] = "Ciao!";
+static uint8_t msgData[40] = "Ciao!";
 
 const unsigned TX_INTERVAL = 60;
 
@@ -77,7 +82,7 @@ void onEvent (ev_t ev) {
                 printf("OP_TXRXPEND, not sending");
             } else {
                 // Prepare upstream data transmission at the next possible time.
-                LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+                LMIC_setTxData2(1, msgData, sizeof(msgData)-1, 0);
                 printf("Packet queued\n");
             }
             break;
@@ -108,6 +113,74 @@ esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
+
+void bmp280_test(void *pvParamters)
+{
+    bmp280_params_t params;
+    bmp280_init_default_params(&params);
+    bmp280_t dev;
+
+    esp_err_t res;
+
+    while (i2cdev_init() != ESP_OK)
+    {
+        printf("Could not init I2Cdev library\n");
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    while (bmp280_init_desc(&dev, BMP280_I2C_ADDRESS_1, I2C_NUM_0, SDA_GPIO, SCL_GPIO) != ESP_OK)
+    {
+        printf("Could not init device descriptor\n");
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    while ((res = bmp280_init(&dev, &params)) != ESP_OK)
+    {
+        printf("Could not init BMP280, err: %d\n", res);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    bool bme280p = dev.id == BME280_CHIP_ID;
+    printf("BMP280: found %s\n", bme280p ? "BME280" : "BMP280");
+
+    float pressure, temperature, humidity;
+
+    while (1)
+    {
+	int b = 0; 
+        int e = 0;
+	int pa = 0;
+        vTaskDelay(TX_INTERVAL * 1000/ portTICK_PERIOD_MS);
+        if (bmp280_read_float(&dev, &temperature, &pressure, &humidity) != ESP_OK)
+        {
+            printf("Temperature/pressure reading failed\n");
+            continue;
+        }
+
+        printf("Pressure: %.2f Pa, Temperature: %.2f C", pressure, temperature);
+	sprintf((char*)msgData,"|%.2f|%.2f|0.00|%i|%i|%i",temperature,pressure,b,e,pa);
+        if (bme280p)
+            printf(", Humidity: %.2f\n", humidity);
+        else
+            printf("\n");
+    }
+}
+
+
+
+void sendMessages(void* pvParameter)
+{
+    if (!(LMIC.opmode & OP_TXRXPEND)) {
+        printf("Sending message...\n");
+	LMIC_setTxData2(1, msgData, sizeof(msgData) - 1,0);	
+        xTaskCreate(bmp280_test, "bmp280_test", 1024 * 4, (void* )0, 3, NULL);
+	}	
+    vTaskDelay(TX_INTERVAL * 1000 / portTICK_PERIOD_MS);
+    
+}
+
+
+/*
 void os_runloop(void) {
 
   if (LMIC.opmode & OP_TXRXPEND) {
@@ -120,7 +193,7 @@ void os_runloop(void) {
  vTaskDelay(TX_INTERVAL * 1000 / portTICK_PERIOD_MS);
 
 }
-
+*/
 void app_main(void)
 {
   os_init();
@@ -152,6 +225,6 @@ void app_main(void)
   //printf("Avvio Join\n");
   //LMIC_startJoining();
   //printf("Join Completo\n");
-  	
-  xTaskCreate(os_runloop, "os_runloop", 1024 * 2, (void* )0, 10, NULL);
+  xTaskCreate(bmp280_test, "bmp280_test", 1024 * 4, (void* )0, 3, NULL);	
+  xTaskCreate(sendMessages, "send_messages", 1024 * 2, (void* )0, 10, NULL);
 }
