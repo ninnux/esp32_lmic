f0de09768eabb6cf10323d50c66f56238e424f1e
LarryN3t <lophone75@gmail.com>
2019-09-14 08:39:42 +0200

    Funzionagit add -A cambiata libreria lmic-esp-idf

--- main/main.c
+++ main/main.c
@@ -12,24 +12,85 @@
 #define SDA_GPIO GPIO_NUM_21
 #define SCL_GPIO GPIO_NUM_22
 
+#define LORA_PIN_SPI_SCLK  CONFIG_LORA_PIN_SPI_SCLK
+#define LORA_PIN_SPI_MOSI  CONFIG_LORA_PIN_SPI_MOSI
+#define LORA_PIN_SPI_MISO  CONFIG_LORA_PIN_SPI_MISO
+#define LORA_PIN_NSS       CONFIG_LORA_PIN_NSS
+//##define LORA_PIN_RXTX      TTN_NOT_CONNECTED
+#define LORA_PIN_RST       CONFIG_LORA_PIN_RST
+#define LORA_PIN_DIO0      CONFIG_LORA_PIN_DIO0
+#define LORA_PIN_DIO1      CONFIG_LORA_PIN_DIO1
+#define LORA_PIN_DIO2      CONFIG_LORA_PIN_DIO2
+
+
+const lmic_pinmap lmic_pins = {
+    .nss = LORA_PIN_NSS ,
+    .rst = LORA_PIN_RST,
+    .dio = {LORA_PIN_DIO0, LORA_PIN_DIO1, LORA_PIN_DIO2},
+    .spi = {LORA_PIN_SPI_MISO,LORA_PIN_SPI_MOSI,LORA_PIN_SPI_SCLK},
+    .rxtx = LMIC_UNUSED_PIN,
+};
+
 unsigned int DevAdd;
 unsigned char NetKey[16];
 unsigned char AppKey[16];
 
 
-u1_t NWKSKEY[16] = { 0xa2, 0x25, 0xb5, 0x22, 0x8f, 0xe5, 0xb8, 0x18, 0xb6, 0x19, 0xc1, 0xb0, 0x9b, 0xc8, 0xdb, 0xd2 };
-u1_t APPSKEY[16] = { 0xfa, 0x4c, 0xd4, 0xd2, 0x78, 0x0d, 0x77, 0xc1, 0x03, 0x82, 0xab, 0x77, 0xdb, 0xd8, 0xe7, 0x00 };
-u4_t DEVADDR = 0x07a56a5e;
+//u1_t NWKSKEY[16] = { 0xa2, 0x25, 0xb5, 0x22, 0x8f, 0xe5, 0xb8, 0x18, 0xb6, 0x19, 0xc1, 0xb0, 0x9b, 0xc8, 0xdb, 0xd2 };
+//u1_t APPSKEY[16] = { 0xfa, 0x4c, 0xd4, 0xd2, 0x78, 0x0d, 0x77, 0xc1, 0x03, 0x82, 0xab, 0x77, 0xdb, 0xd8, 0xe7, 0x00 };
+//u4_t DEVADDR = 0x07a56a5e;
 
 void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-
 static uint8_t msgData[40] = "Ciao!";
 unsigned char mydata[] = "Ciao!";
 
 const unsigned TX_INTERVAL = CONFIG_LORA_TX_INTERVAL;
+
+static osjob_t sendjob;
+
+
+void lorasetup(void) {
+    const char devadd[]= CONFIG_devAdd;
+    const char netkey[]= CONFIG_netKey , *pn = netkey;
+    const char appkey[]= CONFIG_appKey , *pa = appkey;
+/*  
+    for (size_t count = 0; count < sizeof DevAdd/sizeof *DevAdd; count++) {
+        sscanf(da, "%2hhx", &DevAdd[count]);
+        da += 2;
+    }
+*/
+    DevAdd = (int)strtol(devadd,NULL, 16);
+
+    for (size_t count = 0; count < sizeof NetKey/sizeof *NetKey; count++) {
+        sscanf(pn, "%2hhx", &NetKey[count]);
+        pn += 2;
+    }
+    for (size_t count = 0; count < sizeof AppKey/sizeof *AppKey; count++) {
+        sscanf(pa, "%2hhx", &AppKey[count]);
+        pa += 2;
+    }
+}
+
+
+
+
+void sendMessages(osjob_t* j) {
+   lorasetup();
+   LMIC_setSession (0x1, DevAdd, NetKey, AppKey);
+
+  if (LMIC.opmode & OP_TXRXPEND) {
+      printf("OP_TXRXPEND, not sending");
+  } else {
+      // Prepare upstream data transmission at the next possible time.
+      LMIC_setTxData2(1, msgData, sizeof(msgData)-1, 0);
+      printf("Packet sent");
+  }
+
+}
+
 
 void onEvent (ev_t ev) {
     printf("%d", os_getTime());
@@ -71,18 +132,11 @@ void onEvent (ev_t ev) {
               printf(LMIC.dataLen);
               printf(" bytes of payload\n");
             }
-
-            if (LMIC.opmode & OP_TXRXPEND) {
-                printf("OP_TXRXPEND, not sending\n");
-            } else {
-                // Prepare upstream data transmission at the next possible time.
-                LMIC_setTxData2(1, msgData, sizeof(msgData)-1, 0);
-                printf("Packet queued\n");
-            }
-            break;
-	case EV_TXSTART:
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), sendMessages);
+	    break;
+/*	case EV_TXSTART:
 	    printf("EV_TXSTART\n");
-            break;
+            break;*/
         case EV_LOST_TSYNC:
             printf("EV_LOST_TSYNC\n");
             break;
@@ -110,32 +164,6 @@ esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-void sendMessages(void) {
-
-  if (LMIC.opmode & OP_TXRXPEND) {
-      printf("OP_TXRXPEND, not sending");
-  } else {
-      // Prepare upstream data transmission at the next possible time.
-      LMIC_setTxData2(1, msgData, sizeof(msgData)-1, 0);
-      printf("Packet sent");
-  }
-
-}
-
-void msg_test(void *pvParamters)
-{
-	int numes = 0;
-	printf("Eccolo!\n");
-	   while (1)
-    		{
-			printf("Eccolo2\n");
-	//		sprintf((char*)msgData,"Messaggio numero|%i",numes);
-		        sendMessages();	
-			printf("Eccolo1\n");
- //			numes += 1;	
-			vTaskDelay(TX_INTERVAL * 1000/ portTICK_PERIOD_MS);
-		}
-}
 void bmp280_test(void *pvParamters)
 {
     bmp280_params_t params;
@@ -181,7 +209,6 @@ void bmp280_test(void *pvParamters)
 
         printf("Pressure: %.2f Pa, Temperature: %.2f C", pressure, temperature);
 	sprintf((char*)msgData,"|%.2f|%.2f|0.00|%i|%i|%i",temperature,pressure,b,e,pa);
-	sendMessages();
         if (bme280p)
             printf(", Humidity: %.2f\n", humidity);
         else
@@ -189,63 +216,19 @@ void bmp280_test(void *pvParamters)
     }
 }
 
-
-
-
-void lorasetup(void) {
-    const char devadd[]= CONFIG_devAdd;   
-    const char netkey[]= CONFIG_netKey , *pn = netkey;
-    const char appkey[]= CONFIG_appKey , *pa = appkey;
-/*
-    for (size_t count = 0; count < sizeof DevAdd/sizeof *DevAdd; count++) {
-        sscanf(da, "%2hhx", &DevAdd[count]);
-        da += 2;
-    }
-*/
-    DevAdd = (int)strtol(devadd,NULL, 16);
-
-    for (size_t count = 0; count < sizeof NetKey/sizeof *NetKey; count++) {
-        sscanf(pn, "%2hhx", &NetKey[count]);
-        pn += 2;
-    }
-    for (size_t count = 0; count < sizeof AppKey/sizeof *AppKey; count++) {
-        sscanf(pa, "%2hhx", &AppKey[count]);
-        pa += 2;
-    }
-}
-
-
-void os_runloop(void) {
-
-  if (LMIC.opmode & OP_TXRXPEND) {
-      printf("OP_TXRXPEND, not sending");
-  } else {
-      // Prepare upstream data transmission at the next possible time.
-      LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
-      printf("Packet queued");
-  }
-    while(1) {
-    	os_run();
-	vTaskDelay(10 / portTICK_PERIOD_MS);
-// 	vTaskDelay(TX_INTERVAL * 1000 / portTICK_PERIOD_MS);
-	}
-
-}
-
-void app_main(void)
-{
-  os_init();
-
+void LoraStart(void) {
+ // Steup Radio 
   LMIC_reset();
   printf("LMIC RESET\n");
 //  lorasetup();
 //  LMIC_setSession (0x1, DevAdd, NetKey, AppKey);
+   /*
   uint8_t appskey[sizeof(APPSKEY)];
   uint8_t nwkskey[sizeof(NWKSKEY)];
   memcpy(appskey, APPSKEY, sizeof(APPSKEY));
   memcpy(nwkskey, NWKSKEY, sizeof(NWKSKEY));
   LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
-
+*/
   LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
   LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
   LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
@@ -258,15 +241,24 @@ void app_main(void)
 
   LMIC_setLinkCheckMode(0);
   LMIC.dn2Dr = DR_SF9;
-  LMIC.datarate = DR_SF9;
-  LMIC_setDrTxpow(DR_SF9,14);
-  LMIC.txChnl = 0;
+  //LMIC.datarate = DR_SF9;
+  LMIC_setDrTxpow(DR_SF7,14);
+  //LMIC.txChnl = 0;
   //LMIC_setDrTxpow(DR_SF7,14);
 
   for(int i = 1; i <= 8; i++) LMIC_disableChannel(i);
-  //printf("Avvio Join\n");
-  //LMIC_startJoining();
-  //printf("Join Completo\n");
-  //xTaskCreate(bmp280_test, "bmp280_test", 1024 * 4, (void* )0, 3, NULL);	
-  xTaskCreate(os_runloop, "os_runloop", 1024 * 4, (void* )0, 3, NULL);	
+  // Task Loop
+    sendMessages(&sendjob);
+    for (;;) {
+        os_runloop_once();
+        vTaskDelay(1);
+    }
+
+
+	
+}
+void app_main(void)
+{
+  os_init();
+  xTaskCreate(LoraStart, "LoraStart", 1024 * 4, (void* )0, 3, NULL);	
 }
